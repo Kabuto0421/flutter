7ed0fe718e83661617815a8fe31294bc875d5ff3
yudai0421 <kabutoyudai@gmail.com>
2023-03-02 13:44:24 +0900

    First

---
+++ lib/config/config.dart
@@ -0,0 +1,16 @@
+class Configurations {
+  static const _apiKey = "AIzaSyBxqSPqEgnf4PxC9eJgt8XdVVN_ehdsJow";
+  static const _authDomain = "test-14646.firebaseapp.com";
+  static const _projectId = "test-14646";
+  static const _storageBucket = "test-14646.appspot.com";
+  static const _messagingSenderId = "936235803606";
+  static const _appId = "1:936235803606:web:0cdaf621548856dd7d3a77";
+  static const _measurementId = "G-9FSFNC6546";
+  String get apiKey => _apiKey;
+  String get authDomain => _authDomain;
+  String get projectId => _projectId;
+  String get storageBucket => _storageBucket;
+  String get messagingSenderId => _messagingSenderId;
+  String get appId => _appId;
+  String get measurementId => _measurementId;
+}
