d16926834a9624a9b0bf288a45026f31ecf95086
yudai0421 <kabutoyudai@gmail.com>
2023-04-05 16:18:11 +0900

    commit4/5(Wednesday)

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'config/config.dart';
+import 'package:provider/provider.dart';
 
 final configurations = Configurations();
+
 Future<void> main() async {
   // Firebase初期化
   await Firebase.initializeApp(
@@ -39,6 +41,10 @@ class _MyAuthPageState extends State<MyAuthPage> {
   String newUserEmail = "";
   // 入力されたパスワード
   String newUserPassword = "";
+  // 入力されたメールアドレス（ログイン）
+  String loginUserEmail = "";
+  // 入力されたパスワード（ログイン）
+  String loginUserPassword = "";
   // 登録・ログインに関する情報を表示
   String infoText = "";
 
@@ -96,12 +102,225 @@ class _MyAuthPageState extends State<MyAuthPage> {
                 },
                 child: Text("ユーザー登録"),
               ),
+              const SizedBox(height: 32),
+              TextFormField(
+                decoration: InputDecoration(labelText: "メールアドレス"),
+                onChanged: (String value) {
+                  setState(() {
+                    loginUserEmail = value;
+                  });
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: "パスワード"),
+                obscureText: true,
+                onChanged: (String value) {
+                  setState(() {
+                    loginUserPassword = value;
+                  });
+                },
+              ),
               const SizedBox(height: 8),
-              Text(infoText)
+              ElevatedButton(
+                onPressed: () async {
+                  try {
+                    // メール/パスワードでログイン
+                    final FirebaseAuth auth = FirebaseAuth.instance;
+                    final UserCredential result =
+                        await auth.signInWithEmailAndPassword(
+                      email: loginUserEmail,
+                      password: loginUserPassword,
+                    );
+                    // ログインに成功した場合
+                    final User user = result.user!;
+                    setState(() {
+                      infoText = "ログインOK：${user.email}";
+                    });
+                    // ログイン成功時に次の画面に遷移
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => LoggedInHomePage()),
+                    );
+                  } catch (e) {
+                    // ログインに失敗した場合
+                    setState(() {
+                      infoText = "ログインNG：${e.toString()}";
+                    });
+                  }
+                },
+                child: Text("ログイン"),
+              ),
+              const SizedBox(height: 8),
+              Text(infoText),
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class LoggedInHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Home")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "一問一答",
+              style: TextStyle(fontSize: 100, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(width: 300, height: 30),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => StartPage()),
+                );
+              },
+              child: Text(
+                "はじめる",
+                style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(width: 30, height: 30),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => CreatePage()),
+                );
+              },
+              child: Text(
+                "つくる",
+                style: TextStyle(fontSize: 52, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(width: 200, height: 30),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ViewPage()),
+                );
+              },
+              child: Text(
+                "見る",
+                style: TextStyle(fontSize: 52, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class StartPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("はじめる")),
+      body: Center(child: Text("ここは「はじめる」ページです。")),
+    );
+  }
+}
+
+class CreatePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("つくる")),
+      body: Center(child: Text("ここは「つくる」ページです。")),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => CreateDetailPage()),
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class CreateDetailPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("問題作成")),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(
+                labelText: "タイトル",
+                labelStyle: TextStyle(fontSize: 16),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 16),
+            TextField(
+              decoration: InputDecoration(
+                labelText: "教科",
+                labelStyle: TextStyle(fontSize: 16),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 50),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: "問題文",
+                border: OutlineInputBorder(),
+              ),
+              maxLines: 3,
+              keyboardType: TextInputType.multiline,
+            ),
+            SizedBox(height: 16),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: "解答",
+                border: OutlineInputBorder(),
+              ),
+              maxLines: 3,
+              keyboardType: TextInputType.multiline,
+            ),
+            SizedBox(height: 16),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: "解説",
+                border: OutlineInputBorder(),
+              ),
+              maxLines: 2,
+              keyboardType: TextInputType.multiline,
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                // ここに送信処理を追加します
+              },
+              child: Text("送信"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ViewPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("見る")),
+      body: Center(child: Text("ここは「見る」ページです。")),
     );
   }
 }
